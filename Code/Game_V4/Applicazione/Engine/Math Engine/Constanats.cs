e851bc90011936d2e598dbfed25e680e0a649c3f
Deserere <orionenero@gmail.com>
2019-08-10 10:22:39 +0200

    aggiunto converter e constants nel math engine (tutte le costanti saranno da copiare li dentro)

---
+++ Code/Game_V4/Applicazione/Engine/Math Engine/Constanats.cs
@@ -0,0 +1,16 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+public static class Constants
+{
+
+    public static double UA_to_km = 149597870.70;
+
+    public static double UA_to_Mkm = 149597.87070;
+
+    public static double UA_to_LY = 63241.08;
+
+    public static double LY_to_UA = 0.000016;
+}
