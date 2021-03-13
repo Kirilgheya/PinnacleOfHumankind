25763e9093f06a0757a546c80e5fbdcf242d4384
Andrea <andrew.corradi@gmail.com>
2021-03-13 17:53:41 +0100

    Added orbit method

--- Code/GameUI/UI/Utilities/UIStaticClass.cs
+++ Code/GameUI/UI/Utilities/UIStaticClass.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Shapes;
 using GameCore = MainGame.Applicazione.DataModel;
 
@@ -32,6 +34,41 @@ namespace GameUI.UI.Utilities
         public static void show_message(String Message)
         {
             MessageBox.Show(Message);
+        }
+
+        public static void generateOrbitForBody(Canvas _canvas, Ellipse _body,Point _center, Point _bodyCoordinates)
+        {
+
+            Path orbitPath = new Path
+            {
+                Stroke = Brushes.Yellow,
+                StrokeThickness = 2,
+                Fill = Brushes.Transparent
+
+            };
+            EllipseGeometry eg = new EllipseGeometry();
+            double length = Point.Subtract(_center, _bodyCoordinates).Length;
+
+            eg.Center = _center;
+            eg.RadiusX = length - (_body.Width / 2);
+            eg.RadiusY = length - (_body.Width / 2);
+
+            // Add all the geometries to a GeometryGroup.  
+            GeometryGroup orbitGroup = new GeometryGroup();
+            orbitGroup.Children.Add(eg);
+
+            // Set Path.Data  
+            orbitPath.Data = orbitGroup;
+
+
+            Line line = new Line();
+            line.X1 = _center.X;
+            line.X2 = _bodyCoordinates.X;
+            line.Y1 = _center.Y;
+            line.Y2 = _bodyCoordinates.Y;
+
+            _canvas.Children.Add(orbitPath);
+            _canvas.Children.Add(line);
         }
     }
 }
