6a7751ec3216bfdb686dd4a88db7d733bef7a394
Deserere <orionenero@gmail.com>
2021-03-13 13:26:24 +0100

    copmaptibility after merge

--- Code/GameUI/UI/DataSource/Planet.cs
+++ Code/GameUI/UI/DataSource/Planet.cs
@@ -5,6 +5,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using GameCore = MainGame.Applicazione.DataModel;
 namespace GameUI.UI.DataSource
 {
