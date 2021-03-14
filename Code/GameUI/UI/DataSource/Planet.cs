af39b1874057241ab1ca12927e3861cc75ef8466
Andrea <andrew.corradi@gmail.com>
2021-03-14 16:53:19 +0100

    Revert

--- Code/GameUI/UI/DataSource/Planet.cs
+++ Code/GameUI/UI/DataSource/Planet.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using GameCore = MainGame.Applicazione.DataModel;
 namespace GameUI.UI.DataSource
